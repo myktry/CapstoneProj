1ce22af4a86c74502177325c0312686bcbd55f98
myktry <troyshet15@gmail.com>
2026-05-08 16:42:53 +0800

    Fix refund button modal - add missing Alpine.js import and initialization

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,11 @@
-import axios from 'axios';
+﻿import axios from 'axios';
 import flatpickr from 'flatpickr';
+import Alpine from 'alpinejs';
 
 window.axios = axios;
 window.flatpickr = flatpickr;
+window.Alpine = Alpine;
+
+Alpine.start();
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
