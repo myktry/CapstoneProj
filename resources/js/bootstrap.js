a4b77685b01b855cdd11dc79882f3e4aca1ea04e
myktry <troyshet15@gmail.com>
2026-05-16 16:25:23 +0800

    fix admin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,12 +1,7 @@
 ﻿import axios from 'axios';
 import flatpickr from 'flatpickr';
-import Alpine from 'alpinejs';
 
 window.axios = axios;
 window.flatpickr = flatpickr;
-window.Alpine = Alpine;
-
-// Don't start Alpine here - Livewire handles Alpine initialization via @livewireScripts
-// Alpine.start();
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
